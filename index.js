0530908d5cd1add7f2f979f0031d04258fb2a033
simk395 <simon.kong95@gmail.com>
2019-02-21 10:13:32 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function printBadges(array){
+    for(const element of array){
+        //console.log(element);
+        console.log("Welcome " + element + "! You are employee #" + (array.indexOf(element) +1) + '.');
+    }
+    return array;
+}
+
+function tailsNeverFails(){
+    let tails = 0;
+    while(Math.random()>=0.5){
+        tails++;
+    }
+    return ('You got ' + tails + ' tails in a row!');
+}
