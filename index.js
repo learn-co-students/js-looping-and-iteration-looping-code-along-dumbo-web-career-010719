7fc27c78ebba1b7e1ebd31200f2ebf1294b7e499
rchow1944 <rchow2@buffalo.edu>
2019-02-20 20:32:11 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function printBadges(arr) {
+  for (var i = 0; i < arr.length; i++) {
+    console.log(`Welcome ${arr[i]}! You are employee #${i+1}.`)
+  }
+  return arr;
+}
+
+function tailsNeverFails() {
+  let numOfTails = 0;
+  while (Math.random() >= 0.5) {
+    numOfTails++
+  }
+  return `You got ${numOfTails} tails in a row!`
+}
