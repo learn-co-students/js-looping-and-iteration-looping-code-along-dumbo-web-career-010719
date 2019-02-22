17fbc56d6ef42e6e0768da6df2276a6a957853fd
sabud1208 <43481963+sabud1208@users.noreply.github.com>
2019-02-22 00:31:00 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+
+function printBadges(hibitch){
+  for (let i = 0; i < hibitch.length; i++){
+    console.log(`Welcome ${hibitch[i]}! You are employee #${[i+ 1]}.`);
+
+  }
+  return hibitch
+}
+
+function tailsNeverFails(){
+  let tails = 0;
+
+  while  (Math.random() >= 0.5){
+    tails++
+  };
+  return `You got ${tails} tails in a row!`
+}
