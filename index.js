94f7e659ce6de618a5f1db4557960181d43eed58
sbrij001 <sbrij001@yahoo.com>
2019-10-08 11:50:28 -0400

    implemented code for write cards function

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 // Code your solutions in this file
-// In index.js, build a function named writeCards() that accepts two arguments: an array of string names, and an event name. Create a for loop with a counter that starts at 0 and increments at the end of each loop. The condition should halt the for loop after the last name in the array is printed out in the loop body.
-//
-// Inside the loop, create a custom message for each name from the provided array, thanking that person for their gift. Collect the messages in an array and return this array. For example:
+// In index.js, build a function named writeCards() that accepts two arguments: an array of string names, and an event name.
+// Create a for loop with a counter that starts at 0 and increments at the end of each loop. The condition should halt the for loop after the last name in the array is printed out in the loop body.
+// Inside the loop, create a custom message for each name from the provided array, thanking that person for their gift.
+// Collect the messages in an array and return this array.
 
 function writeCards(arr,eventName){
-
+  const cards = [];
+  for (let i = 0; i < arr.length; i++) {
+    cards.push(`Thank you, ${arr[i]}, for the wonderful ${eventName} gift!`)
+  }
+  return cards;
 }
